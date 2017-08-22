f31d858531e3f9231c8cb5a2627becc8beec0ac3
Nethran <30368726+Nethran@users.noreply.github.com>
2017-08-22 15:49:42 +0530

    Update and rename PorNorN to PorNorN.c

--- PorNorN.c
+++ PorNorN.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
 int main()
 {
     int num;
-    
     printf("Enter any number: ");
     scanf("%d", &num);
-    
-
     if(num > 0)
     {
         printf("Number is POSITIVE");
