315ee5f20c6f459121e45b5549c731ce436fd130
Nethran <30368726+Nethran@users.noreply.github.com>
2017-08-22 16:05:38 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+  int supra;
+  printf("enter the value of supera: ");
+  scanf("%d",&supra);
+  if(number % 2 == 0)
+  printf("supra is even");
+  else
+  printf("supra is odd");
+  getch()
+  }
